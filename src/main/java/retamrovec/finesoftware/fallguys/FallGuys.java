ef80a59362f79424332046318d50d849281e6db4
RetamrCZ <59335201+RetamrCZ@users.noreply.github.com>
2022-09-04 12:40:58 +0200

    Initial commit

---
+++ src/main/java/retamrovec/finesoftware/fallguys/FallGuys.java
@@ -0,0 +1,16 @@
+package retamrovec.finesoftware.fallguys;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FallGuys extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

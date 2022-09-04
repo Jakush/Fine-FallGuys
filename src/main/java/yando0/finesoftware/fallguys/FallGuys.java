4fa2ab89cfe6d50dbcd63540545bf7a21bd8f456
RetamrCZ <59335201+RetamrCZ@users.noreply.github.com>
2022-09-04 15:16:20 +0200

    Test

--- src/main/java/yando0/finesoftware/fallguys/FallGuys.java
+++ src/main/java/yando0/finesoftware/fallguys/FallGuys.java
@@ -1,9 +1,12 @@
 package yando0.finesoftware.fallguys;
 
+import org.bukkit.Bukkit;
+
 public class FallGuys {
 
     public void init() {
         // Here import all classes
+        Bukkit.broadcastMessage("Hello world!");
     }
 
 }

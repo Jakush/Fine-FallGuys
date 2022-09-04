a836f2f37df51ec986cb7eead3d487c4b72a7181
RetamrCZ <59335201+RetamrCZ@users.noreply.github.com>
2022-09-04 14:06:04 +0200

    Arena change.

--- src/main/java/retamrovec/finesoftware/fallguys/Managers/Arena.java
+++ src/main/java/retamrovec/finesoftware/fallguys/Managers/Arena.java
@@ -1,4 +1,11 @@
 package retamrovec.finesoftware.fallguys.Managers;
 
+import retamrovec.finesoftware.fallguys.Enums.GameState;
+
 public class Arena {
+
+    private GameState state;
+    public Arena() {
+        this.state = GameState.RECRUITING;
+    }
 }

1b343a276cc68eba77b82362625b6920fcee3fa4
Yando0 <thazacproman123@gmail.com>
2022-09-05 14:26:47 +0300

    Added Event Register

--- src/main/java/yando0/finesoftware/fallguys/FallGuysYando.java
+++ src/main/java/yando0/finesoftware/fallguys/FallGuysYando.java
@@ -1,9 +1,12 @@
 package yando0.finesoftware.fallguys;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
 import retamrovec.finesoftware.fallguys.FallGuys;
 import retamrovec.finesoftware.fallguys.Managers.ConfigManager;
 
@@ -18,8 +21,11 @@ public class FallGuysYando {
 
     public void init() {
         // Here import all classes
-        new ConfigManager("config.yml", this.getDataFolder());
+        new ConfigManager(this.getDataFolder(), "config.yml");
         FallGuys.instance();
+
+        Bukkit.getPluginManager().registerEvents((Listener) this, (Plugin) this);
+
     }
 
     private File getDataFolder() {

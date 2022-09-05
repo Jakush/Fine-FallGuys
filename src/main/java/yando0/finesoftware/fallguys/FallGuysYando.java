2ef1a013c87b54c54537f127199a37c0a5ff787f
RetamrCZ <59335201+RetamrCZ@users.noreply.github.com>
2022-09-05 13:56:42 +0200

    Fixing Yando's main class.

--- src/main/java/yando0/finesoftware/fallguys/FallGuysYando.java
+++ src/main/java/yando0/finesoftware/fallguys/FallGuysYando.java
@@ -1,35 +1,18 @@
 package yando0.finesoftware.fallguys;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.plugin.Plugin;
 import retamrovec.finesoftware.fallguys.FallGuys;
-import retamrovec.finesoftware.fallguys.Managers.ConfigManager;
-
-import java.io.File;
+import yando0.finesoftware.fallguys.Listeners.PlayerJoinListener;
+import yando0.finesoftware.fallguys.Listeners.PlayerLeaveListener;
 
 
 public class FallGuysYando {
 
-
-
-
-
     public void init() {
         // Here import all classes
-        new ConfigManager(this.getDataFolder(), "config.yml");
-        FallGuys.instance();
+        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), FallGuys.instance());
+        Bukkit.getPluginManager().registerEvents(new PlayerLeaveListener(), FallGuys.instance());
 
-        Bukkit.getPluginManager().registerEvents((Listener) this, (Plugin) this);
-
-    }
-
-    private File getDataFolder() {
-        return null;
     }
 
 }

ed605dcd04dcad96f824341cb836677ac703b873
RETAMROVEC <James_Sulc@outlook.cz>
2022-10-07 18:25:13 +0200

    Extended FallGuysAPI. - Added getPlayerBuilder (with all options in that class) in to FallGuysAPI. - Added getCommandSenderBuilder (with all options in that class too) in to FallGuysAPI.

--- src/main/java/retamrovec/finesoftware/fallguys/API/FallGuysAPI.java
+++ src/main/java/retamrovec/finesoftware/fallguys/API/FallGuysAPI.java
@@ -1,6 +1,9 @@
 package retamrovec.finesoftware.fallguys.API;
 
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import retamrovec.finesoftware.fallguys.Builders.CommandSenderBuilder;
+import retamrovec.finesoftware.fallguys.Builders.PlayerBuilder;
 import retamrovec.finesoftware.fallguys.FallGuys;
 import retamrovec.finesoftware.fallguys.Instance.Arena;
 
@@ -11,17 +14,20 @@ public class FallGuysAPI {
     public Arena getArena(Player player) {
         return FallGuys.instance().getArenaManager().getArena(player);
     }
-
     public Arena getArena(int id) {
         return FallGuys.instance().getArenaManager().getArena(id);
     }
-
     public List<Arena> getArenas() {
         return FallGuys.instance().getArenaManager().getArenas();
     }
-
     public ArenaSettings getArenaSettings(Arena arena) {
         return new ArenaSettings(arena);
+    }
+    public PlayerBuilder getPlayerBuilder(Player player) {
+        return new PlayerBuilder(player);
+    }
+    public CommandSenderBuilder getCommandSenderBuilder(CommandSender sender) {
+        return new CommandSenderBuilder(sender);
     }
 
 }
